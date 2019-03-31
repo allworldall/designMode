03e1640d8c42d21a75909ac7c5fa95632510d810
panpan01 <panpan01@meicai.cn>
2019-03-31 16:58:01 +0800

    添加两个文件

---
+++ src/main/java/com/wit/study/decorateDesign/AbstractDecorateMode.java
@@ -0,0 +1,10 @@
+package com.wit.study.decorateDesign;
+
+/**
+ * @Description
+ * @Author panpan
+ * @Date 2019-03-31 16:53
+ * @Version 1.0
+ **/
+public class AbstractDecorateMode {
+}
---
+++ src/main/java/com/wit/study/observeDesign/MyObserve.java
@@ -0,0 +1,17 @@
+package com.wit.study.observeDesign;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * @Description
+ * @Author panpan
+ * @Date 2019-03-31 16:54
+ * @Version 1.0
+ **/
+public class MyObserve implements Observer {
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
